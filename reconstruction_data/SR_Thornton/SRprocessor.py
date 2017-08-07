10a937981b27aef92e22171cf39ce0c87a84956b
xjw1001001 <1176434052@qq.com>
2017-08-07 23:50:33 +0800

    111

--- reconstruction_data/SR_Thornton/SRprocessor.py
+++ reconstruction_data/SR_Thornton/SRprocessor.py
@@ -6,22 +6,22 @@ Created on Mon Aug 07 21:43:37 2017
 """
 
 from Bio import SeqIO
-seq_dictThoronton = SeqIO.to_dict(SeqIO.parse( 'SR213 Final_ER_AR.fasta', "fasta" ))
-seq_dictnucleotide = SeqIO.to_dict(SeqIO.parse( 'ERaERb.fasta', "fasta" ))
-seq_dictThoronton.keys()
-seq_dictThoronton.values()
+seq_dictprotein = SeqIO.to_dict(SeqIO.parse( './ER/processed alignmentER.fasta', "fasta" ))
+seq_dictnucleotide = SeqIO.to_dict(SeqIO.parse( './ER/ERaERb.fasta', "fasta" ))
+seq_dictprotein.keys()
+seq_dictprotein.values()
 
 bases = 'tcag'.upper()
 codons = [a+b+c for a in bases for b in bases for c in bases]
 amino_acids = 'FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG'
 
-codon_table    = dict(zip(codons, amino_acids))
-codon_nonstop  = [a for a in self.codon_table.keys() if not codon_table[a]=='*']
+codon_to_protein    = dict(zip(codons, amino_acids))
+codon_nonstop  = [a for a in codon_to_protein.keys() if not codon_to_protein[a]=='*']
 codon_to_state = {a.upper() : i for (i, a) in enumerate(codon_nonstop)}
 state_to_codon = {i : a.upper() for (i, a) in enumerate(codon_nonstop)}
 
-for keys in seq_dictThoronton.keys():
-    for i in range(len(seq_dictThoronton[keys].seq)):
-        
-    seq_dictThoronton[keys].seq
-    seq_dictnucleotide[keys].seq
+for keys in seq_dictprotein.keys():
+    for i in range((len(seq_dictnucleotide[keys].seq)+1)/3):
+        seq_dictnucleotide[keys].seq[3*i:3*i+3]
+    seq_dictprotein[keys].seq[i]
+    
