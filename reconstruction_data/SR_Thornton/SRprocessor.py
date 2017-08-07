dfa95dfdf5ab4bcebcb2940eb398f79dfbf4ce1c
xjw1001001 <1176434052@qq.com>
2017-08-08 05:09:48 +0800

    111

--- reconstruction_data/SR_Thornton/SRprocessor.py
+++ reconstruction_data/SR_Thornton/SRprocessor.py
@@ -6,7 +6,7 @@ Created on Mon Aug 07 21:43:37 2017
 """
 
 from Bio import SeqIO
-seq_dictprotein = SeqIO.to_dict(SeqIO.parse( './ER/processed alignmentER.fasta', "fasta" ))
+seq_dictprotein = SeqIO.to_dict(SeqIO.parse('./ER/processed alignmentER.fasta', "fasta" ))
 seq_dictnucleotide = SeqIO.to_dict(SeqIO.parse( './ER/ERaERb.fasta', "fasta" ))
 seq_dictprotein.keys()
 seq_dictprotein.values()
@@ -20,8 +20,54 @@ codon_nonstop  = [a for a in codon_to_protein.keys() if not codon_to_protein[a]=
 codon_to_state = {a.upper() : i for (i, a) in enumerate(codon_nonstop)}
 state_to_codon = {i : a.upper() for (i, a) in enumerate(codon_nonstop)}
 
+species_keys=[]
+Thoronton_keys = []
 for keys in seq_dictprotein.keys():
-    for i in range((len(seq_dictnucleotide[keys].seq)+1)/3):
-        seq_dictnucleotide[keys].seq[3*i:3*i+3]
-    seq_dictprotein[keys].seq[i]
-    
+    if keys[-1] != 'T':
+        species_keys.append(keys)
+        Thoronton_keys.append(keys + 'T')
+
+Thoronton_dict={}
+for keys in Thoronton_keys:
+    Thoronton_dict[keys] = str(seq_dictprotein[keys].seq)
+
+nucleotide_dict={}
+for keys in species_keys:
+    nucleotide_dict[keys] = str(seq_dictnucleotide[keys].seq)
+
+protein_dict={}
+alligned_nucleotide_dict={}
+for keys in species_keys:
+    protein_dict[keys] = str(seq_dictprotein[keys].seq)
+    alligned_nucleotide_dict[keys] = ''
+    alligned_nucleotide_dict[keys + 'T'] = ''
+    j=0
+    for i in range(len(protein_dict[keys])):
+        if protein_dict[keys][i] == '-':
+            alligned_nucleotide_dict[keys]+='---'
+            alligned_nucleotide_dict[keys + 'T']+='---'
+        else:
+            alligned_nucleotide_dict[keys]+=nucleotide_dict[keys][3*j:3*j+3]
+            if protein_dict[keys][i] == Thoronton_dict[keys+'T'][i]:
+                alligned_nucleotide_dict[keys + 'T']+=nucleotide_dict[keys][3*j:3*j+3]
+            else:
+                alligned_nucleotide_dict[keys + 'T']+='---'
+            j=j+1
+
+f1=open('./ER/ERnucleotide.fasta','w+')
+f2=open('./ER/ERnucleotide_Thoronton.fasta','w+')
+for keys in alligned_nucleotide_dict.keys():
+    f1.write('>'+keys+'\n')
+    f1.write(alligned_nucleotide_dict[keys])
+    f1.write('\n')
+
+for keys in Thoronton_keys:
+    f2.write('>'+keys+'\n')
+    f2.write(alligned_nucleotide_dict[keys])
+    f2.write('\n')
+
+f1.close()
+f2.close()
+            
+        
+            
---
+++ test/Ancestral_reconstruction/matrix/untitled2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 08 04:20:56 2017
+
+@author: xjw1001001
+"""
+
+import numpy as np
+a=np.loadtxt('./ExpectedIGCnum/EDN_ECP_MG94_IGC.txt')
