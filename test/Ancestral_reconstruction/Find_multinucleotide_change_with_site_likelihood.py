06b935977569d4bceb33f0c3bc035e585dff72ea
xjw1001001 <1176434052@qq.com>
2017-08-11 22:05:33 +0800

    111

---
+++ test/Ancestral_reconstruction/Find_multinucleotide_change_with_site_likelihood.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 11 21:25:36 2017
+
+@author: xjw1001001
+"""
+
+import numpy as np
+
+path = '/Users/xjw1001001/Documents/GitHub/IGCexpansion2/test/Ancestral_reconstruction/'
+reconstruct_path = 'matrix/reconstruction_likelihood/ancestral_reconstruction_'
+
+
+a=np.loadtxt(path + reconstruct_path + 'EDN_ECP_MG94_IGC_node_0.txt')
