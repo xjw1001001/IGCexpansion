ce60832058cbc9d0c74492c2e8228dcfc35b5668
xjw1001001 <1176434052@qq.com>
2017-08-17 02:03:41 +0800

    111

--- test/Ancestral_reconstruction/Find_multinucleotide_change_with_site_likelihood.py
+++ test/Ancestral_reconstruction/Find_multinucleotide_change_with_site_likelihood.py
@@ -13,13 +13,16 @@ paralog_list = [['YLR406C', 'YDL075W'],#pair#TODO: other data
  ['YER131W', 'YGL189C'], ['YML026C', 'YDR450W'], ['YNL301C', 'YOL120C'], ['YNL069C', 'YIL133C'],
  ['YMR143W', 'YDL083C'], ['YJL177W', 'YKL180W'], ['YBR191W', 'YPL079W'], ['YER074W', 'YIL069C'],
  ['YDR418W', 'YEL054C'], ['YBL087C', 'YER117W'], ['YLR333C', 'YGR027C'], ['YMR142C', 'YDL082W'],
- ['YER102W', 'YBL072C'], ['EDN', 'ECP'] ] 
+ ['YER102W', 'YBL072C'], ['EDN', 'ECP'] ,['ERa', 'ERb']] #['ARa', 'ERa']
 
 Yeast_list = [['YLR406C', 'YDL075W'], ['YER131W', 'YGL189C'],['YML026C', 'YDR450W'], ['YNL301C', 'YOL120C'], ['YNL069C', 'YIL133C'],
  ['YMR143W', 'YDL083C'], ['YJL177W', 'YKL180W'], ['YBR191W', 'YPL079W'], ['YER074W', 'YIL069C'], ['YDR418W', 'YEL054C'], ['YBL087C', 'YER117W'],
  ['YLR333C', 'YGR027C'],['YMR142C', 'YDL082W'], ['YER102W', 'YBL072C'],]
+
 EDNECP_newicktree ='/Users/xjw1001001/Documents/GitHub/IGCexpansion2/reconstruction_data/Zhang2002_data ECPEDN/from gene bank/primate_EDN_ECP.newick'
 Yeast_newicktree = '/Users/xjw1001001/Documents/GitHub/IGCexpansion2/YeastTree.newick'
+ERa_ERb_newicktree = '/Users/xjw1001001/Documents/GitHub/IGCexpansion2/reconstruction_data/SR_Thornton/ER/species.newick'
+ARa_ERa_newicktree = '/Users/xjw1001001/Documents/GitHub/IGCexpansion2/reconstruction_data/SR_Thornton/ARa_ERa/ERa_ARa_species.newick'
 
 bases = 'tcag'.upper()
 codons = [a+b+c for a in bases for b in bases for c in bases]
@@ -267,6 +270,188 @@ for pair in plist:
                                             multi_site_dict['_'.join(pair)][site][branch][event]['node1 otherarg prob'] = dict_all['_'.join(pair)]['tau=0']['likelihood'][site,node_to_num[branch[0]],0]
 
 
+##ERaERb
+plist = [['ERa', 'ERb']]
+tree = ERa_ERb_newicktree
+outgroup = 'Branchiostoma_floridae'
+
+ktree, edge_list, node_to_num = read_newick(tree, 'N1')
+num_to_node = {node_to_num[i]:i for i in node_to_num}
+for pair in plist:
+    site_information_dict['_'.join(pair)] = {}
+    site_multianalysis_dict['_'.join(pair)] = {}
+    for site in site_list_dict['_'.join(pair)]:
+        site_information_dict['_'.join(pair)][site] = {}
+        site_multianalysis_dict['_'.join(pair)][site] = {}        
+        for node in range(len(dict_all['_'.join(pair)]['IGC']['arg'][0,:,0])):
+            site_information_dict['_'.join(pair)][site][num_to_node[node]] = {}
+            #记录似然
+            site_information_dict['_'.join(pair)][site][num_to_node[node]]['IGC'] = {}
+            site_information_dict['_'.join(pair)][site][num_to_node[node]]['tau=0'] = {}
+            for i in range(len(dict_all['_'.join(pair)]['IGC']['likelihood'][site,node,:])):
+                site_information_dict['_'.join(pair)][site][num_to_node[node]]['IGC'][state_to_compositecodons(dict_all['_'.join(pair)]['IGC']['arg'][site,node,i])]=dict_all['_'.join(pair)]['IGC']['likelihood'][site,node,i]
+            
+            for i in range(len(dict_all['_'.join(pair)]['tau=0']['likelihood'][site,node,:])):
+                site_information_dict['_'.join(pair)][site][num_to_node[node]]['tau=0'][state_to_compositecodons(dict_all['_'.join(pair)]['tau=0']['arg'][site,node,i])]=dict_all['_'.join(pair)]['tau=0']['likelihood'][site,node,i]
+        for node in range(len(dict_all['_'.join(pair)]['IGC']['arg'][0,:,0])):
+            site_multianalysis_dict['_'.join(pair)][site][num_to_node[node]] = {}
+            #记录似然
+            site_multianalysis_dict['_'.join(pair)][site][num_to_node[node]]['IGC'] = []
+            for i in range(len(dict_all['_'.join(pair)]['IGC']['likelihood'][site,node,:])):
+                if dict_all['_'.join(pair)]['IGC']['likelihood'][site,node,i]>0.05:
+                    site_multianalysis_dict['_'.join(pair)][site][num_to_node[node]]['IGC'].append((state_to_compositecodons(dict_all['_'.join(pair)]['IGC']['arg'][site,node,i]),dict_all['_'.join(pair)]['IGC']['likelihood'][site,node,i]))
+    
+    #在   site_list_dict 中指示的位点寻找IGC中出现多核酸替换，且疑似IGC，组成 multi_site_dict 
+    multi_site_dict['_'.join(pair)] = {}
+    for site in site_list_dict['_'.join(pair)]:
+        flag1 = 0
+        for branch in edge_list:
+            i0 = len(site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'])
+            i1 = len(site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'])#几个备选
+            for i in range(i0):
+                for j in range(i1):
+                    for k in range(2):
+                        count = 0
+                        for l in range(3):
+                            if site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][k][l]!=site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][k][l]:
+                                count = count + 1
+                        if count >= 2:
+                            if site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][0] != site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][1] and site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][0]==site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][1]:
+                                if site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][0]==site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][0] or site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][1] == site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][1]:
+                                    flag1 = 1
+            if flag1 == 1:
+                break   
+        if flag1 == 1:
+            multi_site_dict['_'.join(pair)][site]={}
+            for branch in edge_list:
+                flag = 0
+                i0 = len(site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'])
+                i1 = len(site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'])#几个备选
+                for i in range(i0):
+                    for j in range(i1):
+                        for k in range(2):
+                            count = 0
+                            for l in range(3):
+                                if site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][k][l]!=site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][k][l]:
+                                    count = count + 1
+                            if count >= 2:
+                                if site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][0] != site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][1] and site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][0]==site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][1]:
+                                    if site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][0]==site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][0] or site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][1] == site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][1]:
+                                        flag = 1
+                if flag == 1:
+                    multi_site_dict['_'.join(pair)][site][branch]={}
+                    for i in range(i0):
+                        for j in range(i1):
+                            for k in range(2):#paralog
+                                count = 0
+                                for l in range(3):
+                                    if site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][k][l]!=site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][k][l]:
+                                        count = count + 1
+                                if count >= 2: 
+                                    if site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][0] != site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][1] and site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][0]==site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][1]:
+                                        if site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][0]==site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][0] or site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][1] == site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][1]:
+                                            event = site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][0] + '->' + site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][0] + ' ' + pair[0]+' '+site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][1] + '->' + site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][1] + ' ' + pair[1]
+                                            multi_site_dict['_'.join(pair)][site][branch][event] = {}
+                                            multi_site_dict['_'.join(pair)][site][branch][event]['node1 prob in IGC'] = site_information_dict['_'.join(pair)][site][branch[0]]['IGC'][(event[0:3],event[8 + len(pair[0]) + 2:8 + len(pair[0]) + 5])]
+                                            multi_site_dict['_'.join(pair)][site][branch][event]['node2 prob in IGC'] = site_information_dict['_'.join(pair)][site][branch[1]]['IGC'][(event[5:8],event[8 + len(pair[0]) + 7:8 + len(pair[0]) + 10])]
+                                            if not (event[0:3],event[8 + len(pair[0]) + 2:8 + len(pair[0]) + 5]) in site_information_dict['_'.join(pair)][site][branch[0]]['tau=0'].keys():
+                                                multi_site_dict['_'.join(pair)][site][branch][event]['node1 prob dif'] = abs(site_information_dict['_'.join(pair)][site][branch[0]]['IGC'][(event[0:3],event[8 + len(pair[0]) + 2:8 + len(pair[0]) + 5])])
+                                            else:
+                                                multi_site_dict['_'.join(pair)][site][branch][event]['node1 prob dif'] = abs(site_information_dict['_'.join(pair)][site][branch[0]]['IGC'][(event[0:3],event[8 + len(pair[0]) + 2:8 + len(pair[0]) + 5])]-site_information_dict['_'.join(pair)][site][branch[0]]['tau=0'][(event[0:3],event[8 + len(pair[0]) + 2:8 + len(pair[0]) + 5])])
+                                            multi_site_dict['_'.join(pair)][site][branch][event]['node1 otherwise arg'] = state_to_compositecodons(dict_all['_'.join(pair)]['tau=0']['arg'][site,node_to_num[branch[0]],0])
+                                            multi_site_dict['_'.join(pair)][site][branch][event]['node1 otherarg prob'] = dict_all['_'.join(pair)]['tau=0']['likelihood'][site,node_to_num[branch[0]],0]
+'''
+##ARaERa
+plist = [['ARa', 'ERa']]
+tree = ARa_ERa_newicktree
+outgroup = 'Mus_musculus'
+
+ktree, edge_list, node_to_num = read_newick(tree, 'N1')
+num_to_node = {node_to_num[i]:i for i in node_to_num}
+for pair in plist:
+    site_information_dict['_'.join(pair)] = {}
+    site_multianalysis_dict['_'.join(pair)] = {}
+    for site in site_list_dict['_'.join(pair)]:
+        site_information_dict['_'.join(pair)][site] = {}
+        site_multianalysis_dict['_'.join(pair)][site] = {}        
+        for node in range(len(dict_all['_'.join(pair)]['IGC']['arg'][0,:,0])):
+            site_information_dict['_'.join(pair)][site][num_to_node[node]] = {}
+            #记录似然
+            site_information_dict['_'.join(pair)][site][num_to_node[node]]['IGC'] = {}
+            site_information_dict['_'.join(pair)][site][num_to_node[node]]['tau=0'] = {}
+            for i in range(len(dict_all['_'.join(pair)]['IGC']['likelihood'][site,node,:])):
+                site_information_dict['_'.join(pair)][site][num_to_node[node]]['IGC'][state_to_compositecodons(dict_all['_'.join(pair)]['IGC']['arg'][site,node,i])]=dict_all['_'.join(pair)]['IGC']['likelihood'][site,node,i]
+            
+            for i in range(len(dict_all['_'.join(pair)]['tau=0']['likelihood'][site,node,:])):
+                site_information_dict['_'.join(pair)][site][num_to_node[node]]['tau=0'][state_to_compositecodons(dict_all['_'.join(pair)]['tau=0']['arg'][site,node,i])]=dict_all['_'.join(pair)]['tau=0']['likelihood'][site,node,i]
+        for node in range(len(dict_all['_'.join(pair)]['IGC']['arg'][0,:,0])):
+            site_multianalysis_dict['_'.join(pair)][site][num_to_node[node]] = {}
+            #记录似然
+            site_multianalysis_dict['_'.join(pair)][site][num_to_node[node]]['IGC'] = []
+            for i in range(len(dict_all['_'.join(pair)]['IGC']['likelihood'][site,node,:])):
+                if dict_all['_'.join(pair)]['IGC']['likelihood'][site,node,i]>0.05:
+                    site_multianalysis_dict['_'.join(pair)][site][num_to_node[node]]['IGC'].append((state_to_compositecodons(dict_all['_'.join(pair)]['IGC']['arg'][site,node,i]),dict_all['_'.join(pair)]['IGC']['likelihood'][site,node,i]))
+    
+    #在   site_list_dict 中指示的位点寻找IGC中出现多核酸替换，且疑似IGC，组成 multi_site_dict 
+    multi_site_dict['_'.join(pair)] = {}
+    for site in site_list_dict['_'.join(pair)]:
+        flag1 = 0
+        for branch in edge_list:
+            i0 = len(site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'])
+            i1 = len(site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'])#几个备选
+            for i in range(i0):
+                for j in range(i1):
+                    for k in range(2):
+                        count = 0
+                        for l in range(3):
+                            if site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][k][l]!=site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][k][l]:
+                                count = count + 1
+                        if count >= 2:
+                            if site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][0] != site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][1] and site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][0]==site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][1]:
+                                if site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][0]==site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][0] or site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][1] == site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][1]:
+                                    flag1 = 1
+            if flag1 == 1:
+                break   
+        if flag1 == 1:
+            multi_site_dict['_'.join(pair)][site]={}
+            for branch in edge_list:
+                flag = 0
+                i0 = len(site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'])
+                i1 = len(site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'])#几个备选
+                for i in range(i0):
+                    for j in range(i1):
+                        for k in range(2):
+                            count = 0
+                            for l in range(3):
+                                if site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][k][l]!=site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][k][l]:
+                                    count = count + 1
+                            if count >= 2:
+                                if site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][0] != site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][1] and site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][0]==site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][1]:
+                                    if site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][0]==site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][0] or site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][1] == site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][1]:
+                                        flag = 1
+                if flag == 1:
+                    multi_site_dict['_'.join(pair)][site][branch]={}
+                    for i in range(i0):
+                        for j in range(i1):
+                            for k in range(2):#paralog
+                                count = 0
+                                for l in range(3):
+                                    if site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][k][l]!=site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][k][l]:
+                                        count = count + 1
+                                if count >= 2: 
+                                    if site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][0] != site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][1] and site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][0]==site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][1]:
+                                        if site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][0]==site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][0] or site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][1] == site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][1]:
+                                            event = site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][0] + '->' + site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][0] + ' ' + pair[0]+' '+site_multianalysis_dict['_'.join(pair)][site][branch[0]]['IGC'][i][0][1] + '->' + site_multianalysis_dict['_'.join(pair)][site][branch[1]]['IGC'][j][0][1] + ' ' + pair[1]
+                                            multi_site_dict['_'.join(pair)][site][branch][event] = {}
+                                            multi_site_dict['_'.join(pair)][site][branch][event]['node1 prob in IGC'] = site_information_dict['_'.join(pair)][site][branch[0]]['IGC'][(event[0:3],event[8 + len(pair[0]) + 2:8 + len(pair[0]) + 5])]
+                                            multi_site_dict['_'.join(pair)][site][branch][event]['node2 prob in IGC'] = site_information_dict['_'.join(pair)][site][branch[1]]['IGC'][(event[5:8],event[8 + len(pair[0]) + 7:8 + len(pair[0]) + 10])]
+                                            if not (event[0:3],event[8 + len(pair[0]) + 2:8 + len(pair[0]) + 5]) in site_information_dict['_'.join(pair)][site][branch[0]]['tau=0'].keys():
+                                                multi_site_dict['_'.join(pair)][site][branch][event]['node1 prob dif'] = abs(site_information_dict['_'.join(pair)][site][branch[0]]['IGC'][(event[0:3],event[8 + len(pair[0]) + 2:8 + len(pair[0]) + 5])])
+                                            else:
+                                                multi_site_dict['_'.join(pair)][site][branch][event]['node1 prob dif'] = abs(site_information_dict['_'.join(pair)][site][branch[0]]['IGC'][(event[0:3],event[8 + len(pair[0]) + 2:8 + len(pair[0]) + 5])]-site_information_dict['_'.join(pair)][site][branch[0]]['tau=0'][(event[0:3],event[8 + len(pair[0]) + 2:8 + len(pair[0]) + 5])])
+                                            multi_site_dict['_'.join(pair)][site][branch][event]['node1 otherwise arg'] = state_to_compositecodons(dict_all['_'.join(pair)]['tau=0']['arg'][site,node_to_num[branch[0]],0])
+                                            multi_site_dict['_'.join(pair)][site][branch][event]['node1 otherarg prob'] = dict_all['_'.join(pair)]['tau=0']['likelihood'][site,node_to_num[branch[0]],0]
+'''
 
 
 #dict_all['_'.join(pair)][model]['arg']
--- test/Ancestral_reconstruction/Find_reconstruct_difference.py
+++ test/Ancestral_reconstruction/Find_reconstruct_difference.py
@@ -11,6 +11,9 @@ import matplotlib.pyplot as plt
 EDNECP_newicktree ='/Users/xjw1001001/Documents/GitHub/IGCexpansion2/reconstruction_data/Zhang2002_data ECPEDN/from gene bank/primate_EDN_ECP.newick'
 Yeast_newicktree = '/Users/xjw1001001/Documents/GitHub/IGCexpansion2/YeastTree.newick'
 series_path = '/Users/xjw1001001/Documents/GitHub/IGCexpansion2/test/Ancestral_reconstruction/'
+ERa_ERb_newicktree = '/Users/xjw1001001/Documents/GitHub/IGCexpansion2/reconstruction_data/SR_Thornton/ER/species.newick'
+ARa_ERa_newicktree = '/Users/xjw1001001/Documents/GitHub/IGCexpansion2/reconstruction_data/SR_Thornton/ARa_ERa/ERa_ARa_species.newick'
+
 
 paralog_list = [['YLR406C', 'YDL075W'],#TODO: other data
  ['YER131W', 'YGL189C'],
@@ -224,6 +227,100 @@ for n in range(len(difference_dic)):
     canvas_list[n].print_figure('/Users/xjw1001001/Documents/GitHub/IGCexpansion2/test/Ancestral_reconstruction/pictures_different/' + '_'.join(pair) + '/' + str(difference_dic[n]+1) + '.jpg',dpi=800)
 
 
+##IGC and PAML and IGC_0 ERaERb
+tree = ERa_ERb_newicktree
+outgroup = 'Branchiostoma_floridae'
+pair = ['ERa', 'ERb']
+ktree, edge_list, node_to_num = read_newick(tree, 'N1')
+
+
+
+seq_dict={}
+Model = ['IGC','IGC_0','PAML']
+
+seq_dict['IGC'] = SeqIO.to_dict(SeqIO.parse( series_path + 'series/ancestral_reconstruction_'+'_'.join(pair)+'_MG94+IGC.fasta', "fasta" ))
+seq_dict['IGC_0'] = SeqIO.to_dict(SeqIO.parse( series_path + 'series/ancestral_reconstruction_'+'_'.join(pair)+'_MG94.fasta', "fasta" ))
+seq_dict['PAML'] = SeqIO.to_dict(SeqIO.parse( series_path + 'PAMLfasta/PAML_'+'_'.join(pair)+'.fasta', "fasta" ))
+
+name_to_seq = {model:{name:str(seq_dict[model][name].seq) for name in seq_dict[model].keys()}for model in Model}
+
+nsites = len(name_to_seq['IGC'][name_to_seq['IGC'].keys()[0]])/3 #amio acids
+
+#对比不同,返回有不同位点记录
+difference_dic = []
+for site in range(nsites):
+    for node in node_to_num.keys():
+        if node == 'N0' or node == outgroup:
+            bool_1=name_to_seq['IGC'][node+pair[0]][3*site:3*site+3]!=name_to_seq['IGC_0'][node+pair[0]][3*site:3*site+3]
+            bool_2=name_to_seq['IGC'][node+pair[0]][3*site:3*site+3]!=name_to_seq['PAML'][node+pair[0]][3*site:3*site+3]
+            bool_3=name_to_seq['PAML'][node+pair[0]][3*site:3*site+3]!=name_to_seq['IGC_0'][node+pair[0]][3*site:3*site+3]
+            if bool_1 or bool_2 or bool_3:
+                difference_dic.append(site)
+                break
+        else:
+            paralog =pair[0]
+            bool_1=name_to_seq['IGC'][node+paralog][3*site:3*site+3]!=name_to_seq['IGC_0'][node+paralog][3*site:3*site+3]
+            bool_2=name_to_seq['IGC'][node+paralog][3*site:3*site+3]!=name_to_seq['PAML'][node+paralog][3*site:3*site+3]
+            bool_3=name_to_seq['PAML'][node+paralog][3*site:3*site+3]!=name_to_seq['IGC_0'][node+paralog][3*site:3*site+3]
+            if bool_1 or bool_2 or bool_3:
+                difference_dic.append(site)
+                break
+            paralog =pair[1]
+            bool_1=name_to_seq['IGC'][node+paralog][3*site:3*site+3]!=name_to_seq['IGC_0'][node+paralog][3*site:3*site+3]
+            bool_2=name_to_seq['IGC'][node+paralog][3*site:3*site+3]!=name_to_seq['PAML'][node+paralog][3*site:3*site+3]
+            bool_3=name_to_seq['PAML'][node+paralog][3*site:3*site+3]!=name_to_seq['IGC_0'][node+paralog][3*site:3*site+3]
+            if bool_1 or bool_2 or bool_3:
+                difference_dic.append(site)
+                break
+        
+#每个位点重建树 EDN_ECP
+tree_dict={}
+for site in difference_dic:
+    tree_dict[site] = {}
+    for model in Model:
+        tree_dict[site][model] = Phylo.read(tree, 'newick')
+        for clade in tree_dict[site][model].get_terminals():
+            if clade.name == 'N0' or clade.name == outgroup:
+                line=name_to_seq[model][clade.name+pair[0]][3*site:3*site+3]
+                clade.name = line
+            else:
+                line=name_to_seq[model][clade.name+pair[0]][3*site:3*site+3] +'\n '+ name_to_seq[model][clade.name+pair[1]][3*site:3*site+3]
+                clade.name = line
+        for clade in tree_dict[site][model].get_nonterminals():
+            if clade.name == 'N0' or clade.name == outgroup:
+                line=name_to_seq[model][clade.name+pair[0]][3*site:3*site+3]
+                clade.name = line
+            else:
+                line=name_to_seq[model][clade.name+pair[0]][3*site:3*site+3] +'\n '+ name_to_seq[model][clade.name+pair[1]][3*site:3*site+3]
+                clade.name = line
+'''            
+Phylo.draw(tree_dict[103]['IGC'])     
+Phylo.draw(tree_dict[103]['IGC_0'])
+Phylo.draw(tree_dict[103]['PAML'])
+'''
+
+# object-oriented plot
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+fig_list = []
+canvas_list = []
+for n in range(len(difference_dic)):
+    fig_list.append(Figure())
+    canvas_list.append(FigureCanvas(fig_list[n]))
+    ax={}
+    ax['tree'] = fig_list[n].add_axes([0.1,0.6,0.3,0.3])# [left, bottom, width, height] 
+    ax['IGC'] = fig_list[n].add_axes([0.6,0.6,0.3,0.3])
+    ax['IGC_0'] = fig_list[n].add_axes([0.1,0.1,0.3,0.3])
+    ax['PAML'] = fig_list[n].add_axes([0.6,0.1,0.3,0.3])
+    for key in Model:
+        ax[key].set_title(key+' site:'+ str(difference_dic[n]+1),fontsize='4')
+        Phylo.draw(tree_dict[difference_dic[n]][key],axes = ax[key])
+    ax['tree'].set_title('tree',fontsize='4')
+    Phylo.draw(Phylo.read(tree,'newick'),axes = ax['tree'])
+    if not os.path.isdir('/Users/xjw1001001/Documents/GitHub/IGCexpansion2/test/Ancestral_reconstruction/pictures_different/' + '_'.join(pair)):
+        os.mkdir('/Users/xjw1001001/Documents/GitHub/IGCexpansion2/test/Ancestral_reconstruction/pictures_different/' + '_'.join(pair))
+    canvas_list[n].print_figure('/Users/xjw1001001/Documents/GitHub/IGCexpansion2/test/Ancestral_reconstruction/pictures_different/' + '_'.join(pair) + '/' + str(difference_dic[n]+1) + '.jpg',dpi=600)
 
 
 
