5334fd55bdfc2f15b2631f2d84559ff43e4a3ec1
xjw1001001 <1176434052@qq.com>
2017-08-19 11:16:38 +0800

    111

--- rebuild_Thornton.sh
+++ rebuild_Thornton.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sbatch -o ./cluster_outs/IGC-%j.out --mail-type=ALL --mail-user=1176434052@qq.com ./ShFiles/Thornton.sh 
+sbatch -o ./cluster_outs/IGC-%j.out --mail-type=ALL --mail-user=1176434052@qq.com ./ShFiles/Thornton1.sh 
+sbatch -o ./cluster_outs/IGC-%j.out --mail-type=ALL --mail-user=1176434052@qq.com ./ShFiles/Thornton2.sh 
