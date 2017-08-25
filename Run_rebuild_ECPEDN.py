c5ab6da95daaa8fc7e015f579e9dea590d51cd95
xjw1001001 <1176434052@qq.com>
2017-08-26 05:57:06 +0800

    111

--- Run_rebuild_ECPEDN.py
+++ Run_rebuild_ECPEDN.py
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     
     #MG94+tau
     MG94_tau = ReCodonGeneconv( newicktree, alignment_file, paralog, Model = 'MG94', Force = Force, clock = None, save_path = './test/save/')
-    
+    '''
     MG94_tau.get_mle(True, True, 0, 'BFGS')
     MG94_tau.site_reconstruction()
     
@@ -38,4 +38,4 @@ if __name__ == '__main__':
     MG94_series = MG94.reconstruction_series
     MG94_likelihooddict = MG94.likelihood_dict
     result = MG94_tau.find_differences_between(MG94_tau_series, MG94_series)
-    
+    '''
