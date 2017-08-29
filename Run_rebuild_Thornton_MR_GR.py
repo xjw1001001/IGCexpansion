b92f220e683b15404e9c0efcbd9c503ff19c6bd0
xjw1001001 <1176434052@qq.com>
2017-08-30 03:52:18 +0800

    111

--- Run_rebuild_Thornton_MR_GR.py
+++ Run_rebuild_Thornton_MR_GR.py
@@ -26,9 +26,9 @@ if __name__ == '__main__':
     
     #MG94+tau
     MG94_tau = ReCodonGeneconv( newicktree, alignment_file, paralog, Model = 'MG94', Force = Force, clock = None, save_path = './test/save/')
-    #MG94_tau.get_mle(True, True, 0, 'BFGS')
+    MG94_tau.get_mle(True, True, 0, 'BFGS')
     MG94_tau.site_reconstruction()
-    #MG94_tau.Expected_tau_for_sitewise_and_branchwise()
+    MG94_tau.Expected_tau_for_sitewise_and_branchwise()
     MG94_tau_series = MG94_tau.reconstruction_series
     MG94_tau_likelihooddict = MG94_tau.likelihood_dict
     
@@ -39,9 +39,9 @@ if __name__ == '__main__':
     
     #MG94
     MG94 = ReCodonGeneconv( newicktree, alignment_file, paralog, Model = 'MG94', Force = {5:0.0}, clock = None, save_path = './test/save/')
-    #MG94.get_mle(True, True, 0, 'BFGS')
+    MG94.get_mle(True, True, 0, 'BFGS')
     MG94.site_reconstruction()#存了fasta,Pamlrebuild要自己处理
-    #MG94.Expected_tau_for_sitewise_and_branchwise()
+    MG94.Expected_tau_for_sitewise_and_branchwise()
     MG94_series = MG94.reconstruction_series
     MG94_likelihooddict = MG94.likelihood_dict
     result = MG94_tau.find_differences_between(MG94_tau_series, MG94_series)
