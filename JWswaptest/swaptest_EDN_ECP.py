1b9527a0c2b5de0c2cd261be5e3c6434c85c5272
xjw1001001 <1176434052@qq.com>
2017-09-07 01:41:37 +0800

    111

--- JWswaptest/swaptest_EDN_ECP.py
+++ JWswaptest/swaptest_EDN_ECP.py
@@ -82,8 +82,8 @@ def main(args):
     
     
             alignment_file = path + species1 + '_' + species2 + '.fasta'
-            if not os.path.isdir('./save/' + species1 + '_' + species2 + '/'):
-                os.mkdir('./save/' + species1 + '_' + species2 + '/')
+            if not os.path.isdir('./save/' + species1 + '_' + species2+ str(args.tau) + '/'):
+                os.mkdir('./save/' + species1 + '_' + species2+ str(args.tau) + '/')
 
             MG94_tau = ReCodonGeneconv( newicktree, alignment_file, paralog, Model = 'MG94', Force = Force, clock = None, save_path = './save/' + species1 + '_' + species2 + str(args.tau) + '/', post_dup = 'N2')
             MG94_tau.tau = args.tau
@@ -91,8 +91,8 @@ def main(args):
             swap_dict[species1 + '_' + species2][str(args.tau)] = np.exp(MG94_tau.x_process)
             
             alignment_file = path + species1 + '_' + species2 + '_swap.fasta'
-            if not os.path.isdir('./save/' + species1 + '_' + species2 + '_swap/'):
-                os.mkdir('./save/' + species1 + '_' + species2 + '_swap/')
+            if not os.path.isdir('./save/' + species1 + '_' + species2+ str(args.tau) + '_swap/'):
+                os.mkdir('./save/' + species1 + '_' + species2+ str(args.tau) + '_swap/')
             MG94_taus = ReCodonGeneconv( newicktree, alignment_file, paralog, Model = 'MG94', Force = Force, clock = None, save_path = './save/'+ species1 + '_' + species2 + str(args.tau) + '_swap/', post_dup = 'N2')
             MG94_taus.tau = args.tau
             MG94_taus.get_mle(True, True, 0, 'BFGS')
